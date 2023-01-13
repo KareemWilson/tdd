6a2ee25c630853295a2d3059d8d7e7673ce36ff1
KareemWilson <kareemmagdywilson@gmail.com>
2023-01-14 00:21:00 +0200

    Fix Linter Errors

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
 class Solver
-    def factorial(number)
-      factorial = 1
-      raise 'Number is less than 0' if number.negative?
-  
-      (1..number).each do |n|
-        factorial *= n
-      end
-      factorial
+  def factorial(number)
+    factorial = 1
+    raise 'Number is less than 0' if number.negative?
+
+    (1..number).each do |n|
+      factorial *= n
     end
-  
-    def reversed(str)
-      str.reverse
+    factorial
+  end
+
+  def reversed(str)
+    str.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 5).zero? && (number % 3).zero?
+      'fizzbuzz'
+    elsif (number % 5).zero?
+      'buzz'
+    elsif (number % 3).zero?
+      'fizz'
+    else
+      number.to_s
     end
-  
-    def fizzbuzz(number)
-      if (number % 5).zero? && (number % 3).zero?
-        'fizzbuzz'
-      elsif (number % 5).zero?
-        'buzz'
-  
-      elsif (number % 3).zero?
-        'fizz'
-      else
-        number.to_s
-      end
-    end
+  end
 end
